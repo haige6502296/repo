add7ef7e9e15ce29f3a38d12a4718f1b11c81bd9
amau <446836638@qq.com>
2019-02-26 14:18:40 +0700

    新增了删除,修改和查询的方法

--- Spring_Dao_PreparedStatementCallback/src/net/lct/EmployeeDao.java
+++ Spring_Dao_PreparedStatementCallback/src/net/lct/EmployeeDao.java
@@ -5,18 +5,15 @@ import org.springframework.jdbc.core.PreparedStatementCallback;
 
 public class EmployeeDao {
   private JdbcTemplate jdbctemplate;
+
   public void setJdbctemplate(JdbcTemplate jdbctemplate) {
     this.jdbctemplate = jdbctemplate;
   }
 
-  //编写一个预处理方法
-  void saveData(final Employee e) {
+  //编写一个插入数据的方法
+  void insertData(final Employee e) {//final:不允许被继承
     String query = "insert into employee values(?,?,?)";
-
-    /*下面new接口其实是生产一个匿名类(和接口名相同)临时用了就扔了*/
-    //重写PreparedStatementCallback接口的doInPreparedStatement方法
     jdbctemplate.execute(query, (PreparedStatementCallback<Boolean>) ps -> {
-
       //原接口并没有下面这些语句所以是叫覆盖重写该方法
       ps.setInt(1, e.getId());
       ps.setString(2, e.getName());
@@ -24,4 +21,39 @@ public class EmployeeDao {
       return ps.execute();
     });
   }
+
+  //编写一个修改数据的方法
+  void updateData(final Employee e) {
+    String sql = "update employee set name=?,salary=? where id=?";
+//把Boolean写成了boolean,编译不通过,气死
+    jdbctemplate.execute(sql,(PreparedStatementCallback<Boolean>) ps->{
+      ps.setString(1, e.getName());
+      ps.setFloat(2, e.getSalary());
+      ps.setInt(3, e.getId());
+      return ps.execute();
+    });
+  }
+
+  //编写一个删除数据的方法
+  void deleteData(final Employee e){
+    String sql="delete from employee where id=?";
+    jdbctemplate.execute(sql,(PreparedStatementCallback<Boolean>) ps->{
+      ps.setInt(1, e.getId());
+      return ps.execute();
+    });
+  }
+// 编写一个查询数据的方法
+  void selectData( Employee e){
+    String sql="select * from employee where id between 1 and ?";
+    jdbctemplate.execute(sql,(PreparedStatementCallback<Boolean>) ps->{
+      ps.setInt(1,e.getId());
+      return ps.execute();
+    });
+  }
+
+
+
+
+
+
 }
--- Spring_Dao_PreparedStatementCallback/src/net/lct/MainApp.java
+++ Spring_Dao_PreparedStatementCallback/src/net/lct/MainApp.java
@@ -9,7 +9,18 @@ public class MainApp {
     EmployeeDao edao=(EmployeeDao)ac.getBean("edao");
 
     /*调用dao的方法*/
-    edao.saveData(new Employee(2,"Bush",35000));
-    System.out.println("数据插入成功" );
+    System.out.println();
+    try {
+      edao.insertData(new Employee(2,"Bush",35000));
+      System.out.println("数据插入成功!");
+    } catch (Exception e) {
+      System.out.println("数据插入失败,id重复!" );
+    }
+//数据修改
+edao.updateData(new Employee(107,"Marry",7200));
+    System.out.println("数据修改成功!");
+//数据删除
+    Employee e=new Employee();
+    e.setId(107);
   }
 }
