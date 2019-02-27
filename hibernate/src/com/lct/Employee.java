28a44902aa1b2eedb6695a9b38ad971169c67d56
amau <446836638@qq.com>
2019-02-27 14:34:23 +0700

    调整了文件的相对位置

--- hibernate/src/com/lct/Employee.java
+++ hibernate/src/com/lct/Employee.java
@@ -34,19 +34,4 @@ public class Employee {
     this.salary = salary;
     return this;
   }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Employee employee = (Employee) o;
-    return id == employee.id &&
-            Objects.equals(name, employee.name) &&
-            Objects.equals(salary, employee.salary);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name, salary);
-  }
 }
--- hibernate/src/com/lct/MainApp.java
+++ hibernate/src/com/lct/MainApp.java
@@ -1,3 +1,5 @@
+package com.lct;
+
 import com.lct.Employee;
 import org.hibernate.*;
 import org.hibernate.boot.Metadata;
@@ -6,19 +8,18 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 
-public class Main {
+public class MainApp {
   public static void main( String[] args )
   {
     StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
     Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
-
     SessionFactory factory = meta.getSessionFactoryBuilder().build();
     Session session = factory.openSession();
     Transaction t = session.beginTransaction();
 
     Employee e=new Employee();
     e.setId(3);
-    e.setName("Gaurav");
+    e.setName("Alice");
     e.setSalary(28000);
 
     session.save(e);
