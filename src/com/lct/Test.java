aff8e8f6e55dc1a1c8ac643832c2d11a45c05d26
amaus <446836638@qq.com>
2019-11-14 00:54:19 +0700

    Test Git

---
+++ src/com/lct/Test.java
@@ -0,0 +1,24 @@
+package com.lct;
+
+import java.util.Scanner;
+
+public class Test {
+  public static void main(String[] args) {
+
+    String original;  // Objects of String class
+    StringBuilder reverse = new StringBuilder();
+    StringBuilder temp=null;
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter a string/number to check if it is a palindrome");
+    original = in.nextLine();
+    int length = original.length();
+    for (int i = length - 1; i >= 0; i--)
+      /*reverse.append(original.charAt(i));*/
+   temp=reverse.append(original.charAt(i));
+    System.out.println("temp:"+temp);
+    if (original.equals(reverse.toString()))
+      System.out.println("Entered string/number is a palindrome.");
+    else
+      System.out.println("Entered string/number isn't a palindrome.");
+  }
+}
---
+++ src/com/lct/TestStudent4.java
@@ -0,0 +1,25 @@
+package com.lct;
+
+//定义了一个Student类，并没有创建内存空间，因为对象还没有
+class Student{
+  int rollno;
+  String name;
+  void insertRecord(int r, String n){
+    rollno=r;
+    name=n;
+  }
+  void displayInformation(){System.out.println(rollno+" "+name);}
+}
+
+class TestStudent4{
+  public static void main(String[] args){
+    Student s1=new Student();//new Student()是一个创造出来的对象，被s1这个变量引用了，所以s1是这个new Student()对象的一个引用变量，这个对象存在，但是没有数据
+    System.out.println(s1);
+    Student s2=new Student();//new Student()又是一个创造出来的对象，被s2这个变量引用了，所以s1是这个new Student()对象的一个引用变量,这个对象存在，但是没有数据
+    s1.insertRecord(111,"Karan");// initializing the value to these objects by invoking the insertRecord method
+    System.out.println("s1实例变量赋值后："+s1);
+    s2.insertRecord(222,"Aryan");// initializing the value to these objects by invoking the insertRecord method
+    s1.displayInformation();// Here, we are displaying the state (data) of the objects by invoking the displayInformation() method.
+    s2.displayInformation();
+  }
+}
